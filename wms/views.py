cb43e982c8ec6e15a367fe23fbd1421a690a0e47
Edouard Gonnu <edouard.gonnu@gmail.com>
2026-01-02 16:02:02 +0100

    Bump scan service worker cache for studio nav fix

--- wms/views.py
+++ wms/views.py
@@ -3360,7 +3360,7 @@ def scan_faq(request):
     )
 
 
-SERVICE_WORKER_JS = """const CACHE_NAME = 'wms-scan-v30';
+SERVICE_WORKER_JS = """const CACHE_NAME = 'wms-scan-v31';
 const ASSETS = [
   '/scan/',
   '/static/scan/scan.css',
