2d741464989a0e4c82e4402167a8706ad001efb6
Edouard Gonnu <edouard.gonnu@gmail.com>
2026-05-05 13:33:29 +0200

    fix: include portal onboarding in operational reset

--- wms/reset_operational_data.py
+++ wms/reset_operational_data.py
@@ -136,6 +136,7 @@ DELETE_BATCHES = (
             "wms.ShipmentTrackingAccessGrant",
             "wms.PublicAccountRequest",
             "wms.VolunteerAccountRequest",
+            "wms.PortalOnboardingPreference",
             "wms.PortalAccessGrant",
             "wms.AssociationPortalContact",
             "wms.AssociationRecipient",
